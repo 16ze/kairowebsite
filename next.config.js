69d82da188b517d3b9f64a61ca4f87dbec482789
16ze <noreply@github.com>
2025-04-09 21:49:43 +0200

    Suppression de la configuration Vercel pour consolider sur Heroku uniquement

--- next.config.js
+++ next.config.js
@@ -2,12 +2,27 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "www.kairo-digital.fr"],
+    unoptimized: true,
   },
   experimental: {
     serverActions: {
       allowedOrigins: ["localhost:3000", "www.kairo-digital.fr"],
     },
+  },
+  staticPageGenerationTimeout: 180,
+  async headers() {
+    return [
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
